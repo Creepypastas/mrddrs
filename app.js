56ce4cff0cea9868836c2062e6017d254b223306
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2016-01-18 20:14:24 -0600

    Primeras líneas para mrddrs.creepypastas.com: /index.html	una descripción general /envios.html	la tabla de nuevos envíos

---
+++ app.js
@@ -0,0 +1,25 @@
+angular.module('mrddrs.creepypastas.com', [])
+.controller('posts-nuevos-CTRL', ['$http', function($http) {
+
+  var mrddrs = this;
+  mrddrs.posts = {};
+  mrddrs.posts.get = function(){
+    mrddrs.posts.nuevos = [
+      {
+        ID:99231,
+        post_title:'Nakhiri te persigue.',
+        post_author:'',
+        url:'https://creepypastas.com/nakhiri-te-persigue.html',
+        post_status:'nuevo'
+      }
+    ];
+
+    $http.get('https://creepypastas.com/wdgts/mrddrs.creepypastas.com/nuevo.json')
+    .then(function(res){
+      mrddrs.posts.nuevos = res.data;
+    });
+
+  };
+
+  mrddrs.posts.get();
+}]);
